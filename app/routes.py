e087999451873697f59c4775141c17544a194eb3
Benjaminglt1 <Benjamin.Gallet@etu.univ-savoie.fr>
2020-01-28 11:33:15 +0100

    suppression site

--- app/routes.py
+++
@@ -1,8 +0,0 @@
-from app import app
-from flask import render_template
-
-@app.route('/')
-@app.route('/index')
-@app.route('/login')
-def index():
-    return render_template('index.html')
--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
-class Config(object):
-    # SQLALCHEMY
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
