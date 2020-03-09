fc8d4ac1eed6024d9e59ca0a520703ff86b44cb2
benjaminglt1 <59864076+benjaminglt1@users.noreply.github.com>
2020-03-09 09:40:02 +0100

    Pour le projet propre

--- app/routes.py
+++ app/routes.py
@@ -40,13 +40,15 @@ def pageGenerale():
     query = ("SELECT * FROM etudiant")
     cursor.execute(query)
     etu = cursor.fetchall()
+    for e in etu:
+        print(str(e[0]))
+
+
 
     query = ("SELECT * FROM presence")
     cursor.execute(query)
     presence = cursor.fetchall()
-    #etu = Etudiant.query.all()
-    #presence = Presence.query.all()
-
+    
     return render_template("pageGenerale.html",user=etu,presence=presence)
 
 
@@ -54,7 +56,11 @@ def pageGenerale():
 def pageEtu(id):
     id=id
     if request.method == "POST":
-        idCarteEtu = int(request.form["idCarteEtu"])
+        #idCarteEtu = int(request.form["idCarteEtu"],16)
+        #print("idCarteEtu => "+str(idCarteEtu))
+        #idC = idCarteEtu
+        #print("idCarte= "+str(idC))
+        idCarteEtu = id
         nom = request.form["nom"]
         prenom = request.form["prenom"]
         numeroEtudiant = int(request.form["numeroEtudiant"])
@@ -64,41 +70,50 @@ def pageEtu(id):
         numeroTel = int(request.form["numeroTel"])
         mailEtu = request.form["mailEtu"]
         mailEntreprise = request.form["mailEntreprise"]	
-        print(nom)
-        print(prenom)
+        
+        #print(idCarteEtu)
+        #print(nom)
+        #print(prenom)
         querya = ("UPDATE etudiant SET idCarteEtu=%s,nom=%s,prenom=%s,numeroEtudiant=%s,typeContratEtudiant=%s,tarif=%s,filiere=%s,numeroTel=%s,mailEtu=%s,mailEntreprise=%s WHERE idCarteEtu=%s")
         val = (idCarteEtu,nom,prenom,numeroEtudiant,typeContratEtudiant,tarif,filiere,numeroTel,mailEtu,mailEntreprise,id)
-        #querya = ("UPDATE etudiant SET nom = %s WHERE idCarteEtu=%s")
+        
+        queryb = ("UPDATE presence SET idCarteEtu=%s WHERE idCarteEtu=%s")
+        valb = (idCarteEtu,id)
+	#querya = ("UPDATE etudiant SET nom = %s WHERE idCarteEtu=%s")
         #val = (nom,id)
+        print("1")
         try:
+            print("3")
             cursora.execute(querya,val)
+            #cursorb.execute(queryb,valb)
             cnx.commit()
+            print("2")
+            
             querya = ("SELECT * FROM etudiant WHERE idCarteEtu="+str(id))
             cursora.execute(querya)
             etu = cursora.fetchall()
-            for e in etu:
-                print(e[1])
+            print("4")
 
-            print("try")
+            queryb = ("SELECT * FROM presence WHERE idCarteEtu="+str(id))
+            cursorb.execute(queryb)
+            presence = cursorb.fetchall()
+            print("5")
+
+            return render_template("pageEtu.html",user=etu,presence=presence)
+        
         except:
             print("except")
             cnx.rollback()
-    
+        
     querya = ("SELECT * FROM etudiant WHERE idCarteEtu="+str(id))
     cursora.execute(querya)
     etu = cursora.fetchall()
-    #for e in etu:
-        #print(e[1])
-    #etu = Etudiant.query.get(int(id))
-    #presence ='TEST'
+ 
     
     queryb = ("SELECT * FROM presence WHERE idCarteEtu="+str(id))
     cursorb.execute(queryb)
     presence = cursorb.fetchall()
-    #for p in presence:
-        #print(p[1])
-    #presence = Presence.query.filter_by(idCarteEtu=int(id)).all()
-    #print(presence[0].date)
+
     return render_template("pageEtu.html", user=etu , presence=presence)
 
 @app.route("/pageConvention/<id>", methods=["GET", "POST"])
@@ -126,7 +141,7 @@ def pageModifEtu(id):
     querya = ("SELECT * FROM etudiant WHERE idCarteEtu="+str(id))
     cursora.execute(querya)
     etu = cursora.fetchall()
-
+    #etu[0][0]= hex(etu[0][0])
     return render_template("pageModifEtu.html", user=etu)
 
 @app.route("/pdfEtu/<id>")
