94502ce27b84757b02d9b9489a8b322f1b0b2e59
FrDarky <32299781+FrDarky@users.noreply.github.com>
2020-03-24 11:32:54 +0100

    Déconnexion + ajout redirections si non connecté

--- app/routes.py
+++ app/routes.py
@@ -15,18 +15,49 @@ cnx = mysql.connector.connect(host='192.168.176.21',database='badgeuse',user='be
 cursor = cnx.cursor()
 cursora = cnx.cursor()
 cursorb = cnx.cursor()
+
+# Vérification de la validité du cookie
+def cookieEstValide():
+    # Vérification de connexion déjà valable (cookies)
+    if ('compteConnecte' in request.cookies) and ('typeCompte' in request.cookies):
+        # On vérifie que le compte est un compte valide
+        user_name = request.cookies.get('compteConnecte')
+        user_accType = request.cookies.get('typeCompte')
+        query = ("SELECT identifiant FROM connexion WHERE identifiant=%s AND typeCompte=%s")
+        val = (user_name, user_accType)
+        cursor.execute(query, val)
+        user_result = cursor.fetchall()
+
+        if len(user_result) != 0:
+            # Cookie correct
+            return True
+
+    # Cookie incorrect
+    return False
+
+
+# Vérification que le compte connecté est administrateur
+def compteEstAdmin():
+    if 'typeCompte' in request.cookies:
+        if request.cookies.get('typeCompte') == "administrateur":
+            return True
+    
+    # Non connecté ou non admin
+    return False
+
+
+
 @app.route("/", methods=["GET", "POST"])
 @app.route("/index", methods=["GET", "POST"])
 def index():
+
+
+    if cookieEstValide():
+        return redirect("choixFiliere")
+        
+    
     # CONNEXION
     if request.method == "POST":
-        #--------------------
-        # Première vérification: Déjà connecté à un compte
-        #--------------------
-
-        # if 'compteConnecte' in request.cookies:
-        #     return redirect(choixFiliere)
-        
         
         #--------------------
         # Voir la connexion a partir de la bdd
@@ -58,22 +89,36 @@ def index():
 
 @app.route("/deconnexion", methods=["GET"])
 def deconnexion():
-    res = make_response("Déconnexion réalisée.")
-    res.set_cookie('compteConnecte', "null", max_age=0)
-    res.set_cookie('typeCompte', "null", max_age=0)
-    return res
+    if 'compteConnecte' in request.cookies:
+        res = make_response(render_template('deconnexion.html'))
+        res.set_cookie('compteConnecte', "null", max_age=0)
+        res.set_cookie('typeCompte', "null", max_age=0)
+        return res
+    else:
+        return redirect("index")
 
 @app.route("/choixFiliere", methods=["GET","POST"])
 def choixFiliere():
+
+    # Validation du compte dans le cookie
+    if not cookieEstValide():
+        return redirect("index")
+
     if request.method == "POST":
         tabSemA = request.form["tsa"]
         
         print("tabAlternant"+tabSemA)
 
-    return render_template("choixFiliere.html", connexionError=False)
+    return render_template("choixFiliere.html")
+
 
 @app.route("/pageGenerale")
 def pageGenerale():
+
+    # Validation du compte dans le cookie
+    if not cookieEstValide():
+        return redirect("index")
+
     query = ("SELECT * FROM etudiant")
     cursor.execute(query)
     etu = cursor.fetchall()
@@ -93,6 +138,11 @@ def pageGenerale():
 
 @app.route("/pageEtu/<id>", methods=["GET", "POST"])
 def pageEtu(id):
+
+    # Validation du compte dans le cookie
+    if not cookieEstValide():
+        return redirect("index")
+
     id=id
     if request.method == "POST":
         #idCarteEtu = int(request.form["idCarteEtu"],16)
@@ -157,6 +207,12 @@ def pageEtu(id):
 
 @app.route("/pageConvention/<id>", methods=["GET", "POST"])
 def pageConvention(id):
+
+    # Validation du compte dans le cookie
+    if not cookieEstValide():
+        return redirect("index")
+
+
     id=id
     querya = ("SELECT * FROM etudiant WHERE idCarteEtu="+str(id))
     cursora.execute(querya)
@@ -176,6 +232,12 @@ def pageConvention(id):
 
 @app.route("/pageModifEtu/<id>")
 def pageModifEtu(id):
+
+    # Validation du compte dans le cookie
+    if not cookieEstValide():
+        return redirect("index")
+
+
     id=id
     querya = ("SELECT * FROM etudiant WHERE idCarteEtu="+str(id))
     cursora.execute(querya)
@@ -185,6 +247,12 @@ def pageModifEtu(id):
 
 @app.route("/pdfEtuPresence/<id>")
 def pdfEtuPresence(id): 
+
+    # Validation du compte dans le cookie
+    if not cookieEstValide():
+        return redirect("index")
+
+
     id=id
     querya = ("SELECT * FROM etudiant WHERE idCarteEtu="+str(id))
     cursora.execute(querya)
@@ -203,6 +271,12 @@ def pdfEtuPresence(id):
 
 @app.route("/pdfEtu/<id>")
 def pdfEtu(id): 
+
+    # Validation du compte dans le cookie
+    if not cookieEstValide():
+        return redirect("index")
+
+
     id=id 
     querya = ("SELECT * FROM etudiant WHERE idCarteEtu="+str(id))
     cursora.execute(querya)
@@ -222,6 +296,12 @@ def pdfEtu(id):
 
 @app.route("/archiveEtu/<id>")
 def archiveEtu(id):
+
+    # Validation du compte dans le cookie
+    if not cookieEstValide():
+        return redirect("index")
+
+
     import re
     querya = ("SELECT * FROM etudiant WHERE idCarteEtu="+str(id))
     cursora.execute(querya)
@@ -240,4 +320,10 @@ def archiveEtu(id):
 
 @app.route("/administration")
 def administration():
+
+    # Validation du compte dans le cookie
+    if not cookieEstValide() or not compteEstAdmin():
+        return redirect("index")
+
+
     return render_template("administration.html")
