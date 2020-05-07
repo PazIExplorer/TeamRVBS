8a57a29285a0c677c76587aeb8cf2b8952f5485f
FrDarky <32299781+FrDarky@users.noreply.github.com>
2020-05-07 15:27:10 +0200

    fichier config.py, vérifs dans gestionCompte

---
+++ app/pythonScript/config.py
@@ -0,0 +1,7 @@
+###################
+# BASE DE DONNÉES #
+###################
+BDD_host = '192.168.176.21' # Adresse IP du serveur
+BDD_database = 'badgeuse'   # Nom de la base de données à utiliser
+BDD_user = 'ben'            # Identifiant du compte mysql
+BDD_password = 'teamRVBS'   # Mot de passe du compte mysql
